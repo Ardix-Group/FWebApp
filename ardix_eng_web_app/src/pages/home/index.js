212b9d371017fc34433a0ffb90ab3a9fb90febe2
SkyX [ID FR] <89273191+SkyX-ID-FR@users.noreply.github.com>
2023-06-23 14:38:24 +0200

    Add few new things ! 🔥

--- ardix_eng_web_app/src/pages/home/index.js
+++ ardix_eng_web_app/src/pages/home/index.js
@@ -409,18 +409,6 @@ export default function Profile() {
                                         <h3>{item.authors_name}</h3><br/>
                                         <p className="info_span">This user is one of the first to join the platform ! ✨</p>
                                     </div>
-
-                                    <div className="action_bar">
-                                        <div className="item">
-                                            <svg viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                                            <p>0</p>
-                                        </div>
-
-                                        <div className="item">
-                                            <svg viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-                                            <p>0</p>
-                                        </div>
-                                    </div>
                                 </div>
                             ))}
                         </div>
